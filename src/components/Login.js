2f19c2c1f42a452da6df405326b06206389650fd
mikong <4162+mikong@users.noreply.github.com>
2020-01-22 18:29:17 +0800

    feat: Login/signup with GraphQL mutation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,41 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import { AUTH_TOKEN } from '../constants';
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    createUser(
+      name: $name,
+      authProvider: {
+        email: {
+          email: $email,
+          password: $password
+        }
+      }
+    ) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    signInUser(
+      email: {
+        email: $email,
+        password: $password
+      }
+    ) {
+      token
+      user {
+        id
+      }
+    }
+  }
+`;
 
 class Login extends Component {
   state = {
@@ -37,9 +73,37 @@ class Login extends Component {
           />
         </div>
         <div className="flex mt-4">
-          <div className="cursor-pointer mr-2 btn btn-gray" onClick={() => this._confirm()}>
-            {login ? 'login' : 'create account'}
-          </div>
+          {login ? (
+            <Mutation
+              mutation={LOGIN_MUTATION}
+              variables={{ email, password }}
+              onCompleted={data => this._confirm(data)}
+            >
+              {signInUser => (
+                <div
+                  className="cursor-pointer mr-2 btn btn-gray"
+                  onClick={signInUser}
+                >
+                  login
+                </div>
+              )}
+            </Mutation>
+          ) : (
+            <Mutation
+              mutation={SIGNUP_MUTATION}
+              variables={{ email, password, name }}
+              onCompleted={data => this._load_login(data)}
+            >
+              {createUser => (
+                <div
+                  className="cursor-pointer mr-2 btn btn-gray"
+                  onClick={createUser}
+                >
+                  create account
+                </div>
+              )}
+            </Mutation>
+          )}
           <div
             className="cursor-pointer btn btn-gray"
             onClick={() => this.setState({ login: !login })}
@@ -51,8 +115,14 @@ class Login extends Component {
     );
   }
 
-  _confirm = async () => {
-    // TODO
+  _confirm = async data => {
+    const { token } = data.signInUser;
+    this._saveUserData(token);
+    this.props.history.push(`/`);
+  }
+
+  _load_login = async data => {
+    this.setState({ login: true });
   }
 
   _saveUserData = token => {
